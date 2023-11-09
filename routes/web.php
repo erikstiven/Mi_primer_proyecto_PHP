986a774f0ae65431b2987c58a6d73e0cd6d4f033
Erik Quisnia <erikquisnia@gmail.com>
2023-11-09 17:30:08 -0500

    Se agrega el proyecto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    /*return view('welcome');*/
+    return "Hola mundo";
 });
+
+
+Route::get('cursos', function () {
+    /*return view('welcome');*/
+    return "Bienvenidos a mis cursos";
+});
+
+Route::get('cursos/create', function(){
+    return "En esta pagina podras crear un curso";
+});
+
+Route::get('cursos/{curso}', function ($curso) {
+    /*return view('welcome');*/
+    return "Bienvenidos a mis cursos: $curso";
+});
+
