50286717c0526cfe05b3d9ea3f9c903b10849540
erikstiven <erikquisnia@gmail.com>
2023-12-01 01:40:43 -0500

    Migraciones y introduccion de orm y tinker

---
+++ app/Models/Curso.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Curso extends Model
+{
+    use HasFactory;
+
+    // protected $table = "users"; //Con esto usamos el modelo para guardar mediante el tinker a la base de datos
+}
--- database/migrations/2023_12_01_044644_create_cursos_table.php
+++ database/migrations/2023_12_01_044644_create_cursos_table.php
@@ -14,11 +14,7 @@ return new class extends Migration
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar');
-            $table->rememberToken();
+            $table->string('descripcion');
             $table->timestamps();
         });
     }
